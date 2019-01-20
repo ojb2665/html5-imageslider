7893c730ba4db992ff3332434b00ab4b55a41009
ojb2665 <ojb2665@daum.net>
2019-01-20 16:06:52 +0900

    js2 add

--- HT502-imageSlider/js/type2.js
+++ HT502-imageSlider/js/type2.js
@@ -7,12 +7,11 @@ $(document).ready(function() {
     // $("#slider ul li:nth-child(3)").hide();
 
     setInterval(function() {
-        $("#slider ul li:first-of-type").fadeOut();
-        $("#slider ul li:nth-child(2)").fadeIn();
-        $("#slider ul").append($("#slider ul li:first-of-type"));
+        $("#slider ul li:first-of-type").fadeOut(2000).next().fadeIn(1000).end().appendTo("#slider ul");
+        // $("#slider ul li:first-of-type").fadeOut();
+        // $("#slider ul li:nth-child(2)").fadeIn();
+        // $("#slider ul").append($("#slider ul li:first-of-type"));
         
-    },2000);
-
-
+    },3000);
 
 });
